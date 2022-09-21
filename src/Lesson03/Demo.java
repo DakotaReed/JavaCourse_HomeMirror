462b99a5a8efca8d830e7c0c621f3b78dda2eb46
Dakota <dakota.reed.28@gmail.com>
2022-09-21 17:38:51 +0300

    Home Work

---
+++ src/Lesson03/Demo.java
@@ -0,0 +1,62 @@
+package Lesson03;
+
+public class Demo {
+
+    void handleNumbers(int[] array) {
+
+        int minValue = array[0];
+        int maxValue = array[0];
+        int average = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+
+            average += array[i];
+        }
+
+            String n = "Minimal value is: " + minValue + ". Minimal value is: " + maxValue + ". Average is: " + average + ".";
+            //return n;
+        System.out.println(n);
+
+        }
+
+}
+
+        //int maxValue = array[0];
+//        for (int x = 1; x < array.length; x++) {
+//        if (array[x] > maxValue) {
+//                maxValue = array[x];
+//            }
+//        }
+//        return maxValue;
+
+
+
+
+
+
+//    int getMin(int[] ){
+//
+//        int minValue = inputArray[0];
+//        for(int i=1;i<inputArray.length;i++){
+//            if(inputArray[i] < minValue){
+//                minValue = inputArray[i];
+//            }
+//        }
+//        return minValue;
+//
+//        int getMax(int[] inputArray){
+//            int maxValue = inputArray[0];
+//            for(int i=1;i < inputArray.length;i++){
+//                if(inputArray[i] > maxValue){
+//                    maxValue = inputArray[i];
+//                }
+//            }
+//            return maxValue;
+//        }
---
+++ src/Lesson03/DemoMain.java
@@ -0,0 +1,11 @@
+package Lesson03;
+
+public class DemoMain {
+    public static void main(String[] args) {
+
+        Demo arr = new Demo();
+        int[] arr1 = {2, 4, 6, 8, 1, 9, 5, 3, 7};
+        arr.handleNumbers(arr1);
+
+    }
+}
