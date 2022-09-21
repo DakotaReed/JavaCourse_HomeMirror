b8e6378eeb139afd0047cea6ce15d1f53af1693b
Dakota <dakota.reed.28@gmail.com>
2022-09-21 17:55:13 +0300

    Home Work

--- src/Lesson03/Demo.java
+++ src/Lesson03/Demo.java
@@ -36,28 +36,3 @@ public class Demo {
 //            }
 //        }
 //        return maxValue;
-
-
-
-
-
-
-//    int getMin(int[] ){
-//
-//        int minValue = inputArray[0];
-//        for(int i=1;i<inputArray.length;i++){
-//            if(inputArray[i] < minValue){
-//                minValue = inputArray[i];
-//            }
-//        }
-//        return minValue;
-//
-//        int getMax(int[] inputArray){
-//            int maxValue = inputArray[0];
-//            for(int i=1;i < inputArray.length;i++){
-//                if(inputArray[i] > maxValue){
-//                    maxValue = inputArray[i];
-//                }
-//            }
-//            return maxValue;
-//        }
