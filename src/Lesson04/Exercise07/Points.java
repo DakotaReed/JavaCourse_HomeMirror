e1913daa9cf9914bc7b196f683ea5e4935e4b95f
Dakota <dakota.reed.28@gmail.com>
2022-09-24 10:06:48 +0300

    Home Work

--- src/Lesson04/Exercise07/Points.java
+++ src/Lesson04/Exercise07/Points.java
@@ -3,8 +3,6 @@ package Lesson04.Exercise07;
 public abstract class Points {
 
     String result;
-    String _startingPoint;
-    String _finalDestination;
 
     abstract String Passing(String startingPoint, String finalDestination, String wayPoint);
 
