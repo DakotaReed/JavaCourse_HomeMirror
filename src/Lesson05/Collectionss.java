fe5297570c8b96fe12528161f5958203559854cc
Dakota <dakota.reed.28@gmail.com>
2022-10-06 19:06:35 +0300

    Home Work

--- src/Lesson05/Collectionss.java
+++ src/Lesson05/Collectionss.java
@@ -3,7 +3,7 @@ package Lesson05;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Collections {
+public class Collectionss {
     public static void main(String[] args) {
 
         List<String> countriesON = new ArrayList<String>();
